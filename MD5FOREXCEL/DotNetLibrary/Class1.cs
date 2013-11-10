a0b1b164f66943346e4d2ee98b3941a4b6c7e78c
Harutyun <rapirapp@gmail.com>
2013-11-11 01:16:18 +0400

    v.0.1

---
+++ MD5FOREXCEL/DotNetLibrary/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetLibrary
+{
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class DotNetClass
+    {
+        public DotNetClass()
+        {
+
+        }
+
+        public string DotNetMethod(string input)
+        {
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
